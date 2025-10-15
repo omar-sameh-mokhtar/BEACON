14a3f1870d9cc0247c0773d7127f0b15bae3a24c
yousefzaki9993 <yousefzaki9993@gmail.com>
2025-10-15 21:30:57 +0300

    create network page

---
+++ lib/presentation/Network.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Network_page extends StatelessWidget {
+  const Network_page({super.key});
+
+  @override
+  Widget  build(BuildContext context) {
+    return const Placeholder();
+  }
+}
